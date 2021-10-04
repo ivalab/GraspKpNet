b649f9bcba6a176fb610a2b84b1d920c3a6d6dc1
Ruinian Xu <rxu72@gatech.edu>
2021-10-04 15:17:54 -0400

    bug fix

--- src/lib/datasets/dataset/cornell.py
+++ src/lib/datasets/dataset/cornell.py
@@ -22,6 +22,53 @@ class CORNELL(data.Dataset):
                     dtype=np.float32).reshape(1, 1, 3)
     std = np.array([0.104114, 0.113242, 0.089067],
                    dtype=np.float32).reshape(1, 1, 3)
+    zero_anno_list = ['pcd1018r_rgd_preprocessed_98', 'pcd1023r_rgd_preprocessed_97', 'pcd1024r_rgd_preprocessed_52',
+                      'pcd1028r_rgd_preprocessed_119', 'pcd1017r_rgd_preprocessed_120', 'pcd1021r_rgd_preprocessed_54',
+                      'pcd1003r_rgd_preprocessed_76', 'pcd1029r_rgd_preprocessed_74', 'pcd1003r_rgd_preprocessed_24',
+                      'pcd1000r_rgd_preprocessed_110', 'pcd1018r_rgd_preprocessed_34', 'pcd1001r_rgd_preprocessed_84',
+                      'pcd1018r_rgd_preprocessed_123', 'pcd1018r_rgd_preprocessed_118', 'pcd1030r_rgd_preprocessed_110',
+                      'pcd1001r_rgd_preprocessed_5', 'pcd1003r_rgd_preprocessed_102', 'pcd1003r_rgd_preprocessed_96',
+                      'pcd1021r_rgd_preprocessed_113', 'pcd1003r_rgd_preprocessed_64', 'pcd1029r_rgd_preprocessed_86',
+                      'pcd1003r_rgd_preprocessed_66', 'pcd1003r_rgd_preprocessed_100', 'pcd1021r_rgd_preprocessed_62',
+                      'pcd0248r_rgd_preprocessed_11', 'pcd1024r_rgd_preprocessed_63', 'pcd1018r_rgd_preprocessed_107',
+                      'pcd1018r_rgd_preprocessed_35', 'pcd0248r_rgd_preprocessed_85', 'pcd1018r_rgd_preprocessed_9',
+                      'pcd1003r_rgd_preprocessed_54', 'pcd1023r_rgd_preprocessed_70', 'pcd1030r_rgd_preprocessed_7',
+                      'pcd1018r_rgd_preprocessed_19', 'pcd1029r_rgd_preprocessed_92', 'pcd1003r_rgd_preprocessed_68',
+                      'pcd1003r_rgd_preprocessed_81', 'pcd1003r_rgd_preprocessed_120', 'pcd1001r_rgd_preprocessed_63',
+                      'pcd1003r_rgd_preprocessed_108', 'pcd1003r_rgd_preprocessed_28', 'pcd1003r_rgd_preprocessed_50',
+                      'pcd1026r_rgd_preprocessed_16', 'pcd1024r_rgd_preprocessed_13', 'pcd1024r_rgd_preprocessed_75',
+                      'pcd1024r_rgd_preprocessed_50', 'pcd1021r_rgd_preprocessed_104', 'pcd1024r_rgd_preprocessed_10',
+                      'pcd1003r_rgd_preprocessed_67', 'pcd1021r_rgd_preprocessed_117', 'pcd1001r_rgd_preprocessed_86',
+                      'pcd0117r_rgd_preprocessed_4', 'pcd1018r_rgd_preprocessed_74', 'pcd1003r_rgd_preprocessed_88',
+                      'pcd1003r_rgd_preprocessed_94', 'pcd1029r_rgd_preprocessed_15', 'pcd1003r_rgd_preprocessed_1',
+                      'pcd1003r_rgd_preprocessed_95', 'pcd1028r_rgd_preprocessed_26', 'pcd1024r_rgd_preprocessed_27',
+                      'pcd1030r_rgd_preprocessed_75', 'pcd1003r_rgd_preprocessed_98', 'pcd1000r_rgd_preprocessed_26',
+                      'pcd1001r_rgd_preprocessed_108', 'pcd1003r_rgd_preprocessed_59', 'pcd1021r_rgd_preprocessed_96',
+                      'pcd1023r_rgd_preprocessed_122', 'pcd1003r_rgd_preprocessed_125', 'pcd1001r_rgd_preprocessed_80',
+                      'pcd1030r_rgd_preprocessed_111', 'pcd1029r_rgd_preprocessed_34', 'pcd1024r_rgd_preprocessed_70',
+                      'pcd1029r_rgd_preprocessed_94', 'pcd1001r_rgd_preprocessed_93', 'pcd1024r_rgd_preprocessed_84',
+                      'pcd1024r_rgd_preprocessed_22', 'pcd1003r_rgd_preprocessed_111', 'pcd1003r_rgd_preprocessed_110',
+                      'pcd1003r_rgd_preprocessed_63', 'pcd1021r_rgd_preprocessed_46', 'pcd0248r_rgd_preprocessed_95',
+                      'pcd1003r_rgd_preprocessed_15', 'pcd1003r_rgd_preprocessed_17', 'pcd1023r_rgd_preprocessed_124',
+                      'pcd1024r_rgd_preprocessed_73', 'pcd1003r_rgd_preprocessed_93', 'pcd0248r_rgd_preprocessed_81',
+                      'pcd1001r_rgd_preprocessed_13', 'pcd1023r_rgd_preprocessed_18', 'pcd1023r_rgd_preprocessed_55',
+                      'pcd1001r_rgd_preprocessed_66', 'pcd1024r_rgd_preprocessed_115', 'pcd1024r_rgd_preprocessed_24',
+                      'pcd1001r_rgd_preprocessed_75', 'pcd1021r_rgd_preprocessed_7', 'pcd1003r_rgd_preprocessed_31',
+                      'pcd1023r_rgd_preprocessed_35', 'pcd1003r_rgd_preprocessed_70', 'pcd1024r_rgd_preprocessed_40',
+                      'pcd1030r_rgd_preprocessed_88', 'pcd1030r_rgd_preprocessed_13', 'pcd1003r_rgd_preprocessed_113',
+                      'pcd0248r_rgd_preprocessed_75', 'pcd1003r_rgd_preprocessed_5', 'pcd0249r_rgd_preprocessed_64',
+                      'pcd1024r_rgd_preprocessed_34', 'pcd1003r_rgd_preprocessed_27', 'pcd1003r_rgd_preprocessed_23',
+                      'pcd1029r_rgd_preprocessed_64', 'pcd1003r_rgd_preprocessed_39', 'pcd1003r_rgd_preprocessed_43',
+                      'pcd0248r_rgd_preprocessed_34', 'pcd1023r_rgd_preprocessed_88', 'pcd0117r_rgd_preprocessed_102',
+                      'pcd1028r_rgd_preprocessed_57', 'pcd0117r_rgd_preprocessed_98', 'pcd1018r_rgd_preprocessed_71',
+                      'pcd1003r_rgd_preprocessed_36', 'pcd1003r_rgd_preprocessed_57', 'pcd1003r_rgd_preprocessed_42',
+                      'pcd1018r_rgd_preprocessed_56', 'pcd1029r_rgd_preprocessed_122', 'pcd0117r_rgd_preprocessed_18',
+                      'pcd1001r_rgd_preprocessed_10', 'pcd0117r_rgd_preprocessed_43', 'pcd1024r_rgd_preprocessed_97',
+                      'pcd1021r_rgd_preprocessed_109', 'pcd1029r_rgd_preprocessed_69', 'pcd1003r_rgd_preprocessed_116',
+                      'pcd1028r_rgd_preprocessed_14', 'pcd0117r_rgd_preprocessed_40', 'pcd1028r_rgd_preprocessed_76',
+                      'pcd1001r_rgd_preprocessed_14', 'pcd1003r_rgd_preprocessed_84', 'pcd1018r_rgd_preprocessed_113',
+                      'pcd1003r_rgd_preprocessed_14', 'pcd1021r_rgd_preprocessed_102', 'pcd1003r_rgd_preprocessed_3',
+                      'pcd1003r_rgd_preprocessed_75', 'pcd1001r_rgd_preprocessed_36']
 
     def __init__(self, opt, split):
         super(CORNELL, self).__init__()
@@ -62,6 +109,11 @@ class CORNELL(data.Dataset):
             line = f.readline()
             while line:
                 line = line.split('\n')[0]
+
+                if line in self.zero_anno_list:
+                    line = f.readline()
+                    continue
+
                 images.append(line)
                 line = f.readline()
         return images
@@ -74,6 +126,7 @@ class CORNELL(data.Dataset):
         nm_suc_case = 0
         bar = Bar('cornell evaluation', max=dataset_size)
 
+        factor = 256. / 227.
         for ind, (image_id, result) in enumerate(results.items()):
             Bar.suffix = '[{0}/{1}]|Tot: {total:} |ETA: {eta:} '.format(
                 ind, dataset_size, total=bar.elapsed_td, eta=bar.eta_td)
@@ -86,7 +139,9 @@ class CORNELL(data.Dataset):
             with open(anno_path, 'r') as f:
                 line = f.readline().split()
                 while line:
-                    xmin, ymin, xmax, ymax, angle = float(line[1]), float(line[2]), float(line[3]), float(line[4]), float(line[5])
+                    xmin, ymin, xmax, ymax, angle = float(line[1]) * factor, float(line[2]) * factor, \
+                                                    float(line[3]) * factor, float(line[4]) * factor, \
+                                                    float(line[5]) * factor
                     x_c, y_c = (xmin + xmax) / 2, (ymin + ymax) / 2
                     width, height = xmax - xmin, ymax - ymin
 
@@ -142,7 +197,7 @@ class CORNELL(data.Dataset):
                                                    np.ascontiguousarray(boxes_gt[:, :4], dtype=np.float32),
                                                    bbox_pr[:, -1], boxes_gt[:, -1])
 
-            if evaluate(overlaps, bbox_pr, boxes_gt):
+            if self.evaluate(overlaps, bbox_pr, boxes_gt):
                 nm_suc_case += 1
 
             bar.next()
